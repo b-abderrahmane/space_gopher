967c4db0040ab12ac692732b8457574933bb941a
abdou <b-abderrahmane@outlook.com>
2019-10-16 13:55:43 +0200

    minor style fixes

--- pkg/awsstorage/files.go
+++ pkg/awsstorage/files.go
@@ -3,12 +3,13 @@ package awsstorage
 import (
 	"encoding/json"
 	"fmt"
+	"io"
+	"os"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
-	"io"
-	"os"
 )
 
 // BucketManifestFilename is the file storing the information
@@ -30,13 +31,13 @@ func fileExists(sess *session.Session, bucketName string, filename string) bool 
 	return false
 }
 
-func uploadFile(sess *session.Session, bucketName string, file_path string, uploadOverwrite bool, fileContent io.Reader) {
+func uploadFile(sess *session.Session, bucketName string, filePath string, uploadOverwrite bool, fileContent io.Reader) {
 	// Create an uploader with the session and default options
 	uploader := s3manager.NewUploader(sess)
 	// Upload the file to S3.
 	result, err := uploader.Upload(&s3manager.UploadInput{
 		Bucket: aws.String(bucketName),
-		Key:    aws.String(file_path),
+		Key:    aws.String(filePath),
 		Body:   fileContent,
 	})
 	if err != nil {
@@ -45,38 +46,38 @@ func uploadFile(sess *session.Session, bucketName string, file_path string, uplo
 	fmt.Printf("file uploaded to, %s\n", aws.StringValue(&result.Location))
 }
 
-func UploadFile(sess *session.Session, bucketName string, file_path string, uploadOverwrite bool) {
+func UploadFile(sess *session.Session, bucketName string, filePath string, uploadOverwrite bool) {
 
-	fileContent, err := os.Open(file_path)
+	fileContent, err := os.Open(filePath)
 	if err != nil {
-		ExitErrorf("failed to open file %q, %v", file_path, err)
+		ExitErrorf("failed to open file %q, %v", filePath, err)
 	}
 
 	if !fileExists(sess, bucketName, BucketManifestFilename) {
 		fmt.Println("This bucket does not have a manifest file.")
 	}
 
-	if !uploadOverwrite && fileExists(sess, bucketName, file_path) {
-		ExitErrorf("Upload canceled, a file with the same name (%q) already exists", file_path)
+	if !uploadOverwrite && fileExists(sess, bucketName, filePath) {
+		ExitErrorf("Upload canceled, a file with the same name (%q) already exists", filePath)
 	}
-	uploadFile(sess, bucketName, file_path, uploadOverwrite, fileContent)
+	uploadFile(sess, bucketName, filePath, uploadOverwrite, fileContent)
 }
 
-func DownloadFile(sess *session.Session, bucketName string, file_path string) {
+func DownloadFile(sess *session.Session, bucketName string, filePath string) {
 
 	// Create a downloader with the session and default options
 	downloader := s3manager.NewDownloader(sess)
 
 	// Create a file to write the S3 Object contents to.
-	f, err := os.Create(file_path)
+	f, err := os.Create(filePath)
 	if err != nil {
-		ExitErrorf("failed to create file %q, %v", file_path, err)
+		ExitErrorf("failed to create file %q, %v", filePath, err)
 	}
 
 	// Write the contents of S3 Object to the file
 	n, err := downloader.Download(f, &s3.GetObjectInput{
 		Bucket: aws.String(bucketName),
-		Key:    aws.String(file_path),
+		Key:    aws.String(filePath),
 	})
 	if err != nil {
 		ExitErrorf("failed to download file, %v", err)
